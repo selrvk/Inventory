f9968ed3f3cfde7fdab1588171bc60f7f19ba72d
Charles <charles.a7cantara@gmail.com>
2024-10-28 22:46:02 +0800

    initial

---
+++ src/main/java/com/selrvk/inventory/Controller.java
@@ -0,0 +1,12 @@
+package com.selrvk.inventory;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    Button addNewBtn;
+
+
+}
---
+++ src/main/java/com/selrvk/inventory/Main.java
@@ -0,0 +1,27 @@
+package com.selrvk.inventory;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("Main.FXML"));
+        stage.setTitle("Inventory");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+    public static void main(String[] args){
+
+        launch(args);
+
+    }
+}
